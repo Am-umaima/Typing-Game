c669aba6a302974ccfbe561a90f40716d3af0ff3
Oumaima Amlou <128989099+Am-umaima@users.noreply.github.com>
2024-09-29 23:35:17 +0100

    Update script.js

--- SecondModel/Mine/script.js
+++ SecondModel/Mine/script.js
@@ -1,32 +1,95 @@
-let words=["ikchawen","isardonan","ifonasen","yazidan"]
+window.onload = function () {
+    document.getElementById('mots').checked = false;
+    document.getElementById('phrases').checked = false;
+};
 
-// let userSays = prompt("Word or Sentence ?");
-let ZoneTOShow= document.querySelector("span#TheText");
+let wordRadioClicked = document.getElementById('mots');
+let sentenceRadioClicked = document.getElementById('phrases');
 
-function ShowChosed(x) {
+let ZoneToShow = document.querySelector("span#TheText");
+let score = 0;
+let order = 0;
+let words = ["Microphones", "Botatos", "Cows", "Chickens", "kittens"]; 
+let sentences = ["life jamila", "elhawa sultan", "Botatos Koty", "Election of theyear"];
+let currentMode = 'words';  
 
-    
-    if (words[x]===undefined) {
-        GameFinished() 
-    }else
-    {
-        ZoneTOShow.innerHTML=words[x]
+
+wordRadioClicked.addEventListener("click", function () {
+    resetGame();
+    currentMode = 'words'; 
+    ShowWord(order);
+});
+
+sentenceRadioClicked.addEventListener("click", function () {
+    resetGame(); 
+    currentMode = 'sentences';
+    ShowSentence(order);
+});
+
+function ShowWord(x) {
+    if (x >= words.length) {
+        GameFinished();
+        return;
     }
-  
+    ZoneToShow.innerHTML = words[x];  
 }
 
-let order=0;
+function ShowSentence(x) {
+    if (x >= sentences.length) {
+        GameFinished();
+        return;
+    }
+    ZoneToShow.innerHTML = sentences[x]; 
+}
 
-let ButtonClicked= document.getElementById("btnValiderMot");
-ButtonClicked.addEventListener("click",function NextToShow() {
-    ShowChosed(order)
-    order++
-    console.log(`am clicked in ${order}`)
-})
+let ButtonClicked = document.getElementById("btnValiderMot");
+ButtonClicked.addEventListener("click", function () {
+    let inputEc = document.getElementById("inputEcriture").value.trim(); 
+    if (currentMode === 'words') {
+        if (inputEc === words[order]) {
+            score++;  
+        }
+        ShowWord(order + 1);  
+    } else if (currentMode === 'sentences') {
+        if (inputEc === sentences[order]) {
+            score++; 
+        }
+        ShowSentence(order + 1);
+    }
 
-
+    order++; 
+    AddScore(score, (currentMode === 'words' ? words.length : sentences.length)); 
+    document.getElementById("inputEcriture").value = ""; 
+});
 
 function GameFinished() {
-    document.getElementById("btnValiderMot").disabled=true;
-    ZoneTOShow.innerHTML="Game Is Finished ";
+    document.getElementById("btnValiderMot").disabled = true;  
+    ZoneToShow.innerHTML = "Game Is Finished";
 }
+
+function AddScore(value, total) {
+    let affichScore = document.querySelector(".zoneScore span");
+    affichScore.innerHTML = `${value}/${total}`;
+}
+
+function resetGame() {
+    score = 0;
+    order = 0;
+    ZoneToShow.innerHTML = ""; 
+    document.getElementById("btnValiderMot").disabled = false;
+   
+	    let nbr
+	 if (currentMode === 'words') {
+       nbr=words.length
+    } else if (currentMode === 'sentences') {
+        nbr=sentences.length
+    }
+	 AddScore(0, nbr); 
+}
+  function openPopup() {
+            document.getElementById('popupOverlay').classList.add('active');
+        }
+
+        function closePopup() {
+            document.getElementById('popupOverlay').classList.remove('active');
+        }
